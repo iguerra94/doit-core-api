3d3eecf1989f706bbdbf1f60fd327f10e8e1e0cb
Ivan Gabriel Guerra <ivanguerra09@gmail.com>
2025-08-19 13:15:27 -0300

    first commit

---
+++ src/test/java/com/doit/core_api/DoItCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doit.core_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
